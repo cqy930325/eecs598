898feddb21411b14ad639896509b1a7297ca8b4d
Qingyu Chen <chenqingyu93@gmail.com>
2016-10-18 09:05:25 -0400

    partialy done with fk

--- kineval/kineval_forward_kinematics.js
+++ kineval/kineval_forward_kinematics.js
@@ -25,7 +25,58 @@ kineval.robotForwardKinematics = function robotForwardKinematics () {
 
     // STENCIL: implement kineval.buildFKTransforms();
 
+    kineval.buildFKTransforms();
 }
+
+kineval.buildFKTransforms() = function buildFKTransforms(){
+    kineval.traverseFKBase();
+    if (robot.links_geom_imported) {
+        var offset_xform = matrix_multiply(generate_rotation_matrix_Y(-Math.PI/2),generate_rotation_matrix_X(-Math.PI/2));
+        kineval.traverseFKLink(robot.links[robot.base],matrix_multiply(robot.origin.xform,offset_xform));
+    }else{
+        kineval.traverseFKLink(robot.links[robot.base],robot.origin.xform);
+    }
+}
+
+kineval.traverseFKLink() = function traversalFKLink(link,xform){
+    link.xform = matrix_multiply(xform,generate_identity());
+    var i;
+    if (typeof link.children !== 'undefined'){
+        for(i = 0;i<link.children.length,i++){
+            kineval.traverseFKLink(robot.joints[link.children[i]],link.xform);
+        }
+    }else{
+        return;
+    }
+}
+
+kineval.traverseFKJoint() = function traverseFKJoint(joint,xform){
+    var trans = generate_translation_matrix(joint.origin.xyz);
+    var x_rot = generate_rotation_matrix_X(joint.origin.rpy[0]);
+    var y_rot = generate_rotation_matrix_X(joint.origin.rpy[1]);
+    var z_rot = generate_rotation_matrix_X(jointt.origin.rpy[2]);
+    joint_origin_xform = matrix_multiply(trans,matrix_multiply(z_rot,matrix_multiply(y_rot,x_rot)));
+    if(joint.type === "prismatic"){
+        var trans_pris = []
+        trans_pris[0] = joint.angle * joint.axis[0];
+        trans_pris[1] = joint.angle * joint.axis[1];
+        trans_pris[2] = joint.angle * joint.axis[2];
+        joint_xform = generate_translation_matrix(trans_pris);
+    }
+    else if ((joint.type === 'revolute')||(joint.type === 'continuous'))
+}
+
+kineval.traverseFKBase() = function traverseFKBase(){
+    var trans = generate_translation_matrix(robot.origin.xyz);
+    var x_rot = generate_rotation_matrix_X(robot.origin.rpy[0]);
+    var y_rot = generate_rotation_matrix_X(robot.origin.rpy[1]);
+    var z_rot = generate_rotation_matrix_X(robot.origin.rpy[2]);
+    robot.origin.xform = matrix_multiply(trans,matrix_multiply(z_rot,matrix_multiply(y_rot,x_rot)));
+    robot_heading = matrix_multiply(robot.origin.xform,[[0],[0],[1],[1]]);
+    robot_lateral = matrix_multiply(robot.origin.xform,[[1],[0],[0],[1]]);
+}
+
+
 
     // STENCIL: reference code alternates recursive traversal over 
     //   links and joints starting from base, using following functions: 
--- kineval/kineval_matrix.js
+++ kineval/kineval_matrix.js
@@ -17,6 +17,84 @@ function matrix_copy(m1) {
     return mat;
 }
 
+function matrix_multiply(m1, m2) {
+    var ans = [];
+    if (m1[0].length != m2.length){
+        return false;
+    }
+    var i,j,k;
+    for (i=0;i<m1.length;i++) { // for each row of m1
+        ans[i] = [];
+        for (j=0;j<m2[0].length;j++) { // for each column of m1
+            ans[i][j] = 0;
+            for (k=0;k<m1[0].length;k++){
+                ans[i][j]+=m1[i][k]*m2[k][j];
+            }
+        }
+    }
+    return ans;
+}
+
+function matrix_transpose(m1){
+    var ans = [];
+    for (i=0;i<m1.length;i++) { // for each row of m1
+        ans[i] = [];
+        for (j=0;j<m1[0].length;j++) { // for each column of m1
+            ans[i][j] = m1[j][i];
+        }
+    }
+    return ans;
+}
+
+function generate_identity(){
+    var ans = [[1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]];
+    return ans;
+}
+function generate_translation_matrix(xyz){
+    var ans = [[1,0,0,xyz[0]],
+                [0,1,0,xyz[1]],
+                [0,0,1,xyz[2]],
+                [0,0,0,1]];
+    return ans;
+}
+
+function generate_rotation_matrix_X(theta){
+    var cos = Math.cos(theta);
+    var sin = Math.sin(theta);
+    var ans = [[1,0,0,0],
+                [0,cos,-sin,0],
+                [0 sin,cos,0],
+                [0,0,0,1]];
+    return ans;
+
+}
+
+function generate_rotation_matrix_Y(theta){
+    var cos = Math.cos(theta);
+    var sin = Math.sin(theta);
+    var ans = [[cos,0,sin,0],
+                [0,1,0,0],
+                [-sin,0,cos,0],
+                [0,0,0,1]];
+    return ans;
+
+}
+
+function generate_rotation_matrix_Z(theta){
+    var cos = Math.cos(theta);
+    var sin = Math.sin(theta);
+    var ans = [[cos,-sin,0,0],
+                [sin1,cos,0,0],
+                [0,0,1,0],
+                [0,0,0,1]];
+    return ans;
+
+}
+
+
 
     // STENCIL: reference matrix code has the following functions:
     //   matrix_multiply
